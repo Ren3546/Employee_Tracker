f1d32835e7249573ccf0bd14cd6d601d7cb5100a
Rengo _ <renatovaldez6879@gmail.com>
2024-02-01 17:22:10 -0500

    added code to match department id when making a new role, code to update employee role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,17 @@
 INSERT INTO department (name)
-VALUES ('Office'),
-       ('Cash Register'),
-       ('Kitchen');
+VALUES 
+('Office'),
+('Cash Register'),
+('Kitchen');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Management', 1000000, 1),
-       ('Cashier', 50000, 2),
-       ('Chef', 10, 3);
+VALUES 
+('Management', 1000000.00, 1),
+('Cashier', 500.00, 2),
+('Chef', 10.00, 3);
     
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Eugene', 'Krabs', 001, null),
-       ('Squidward', 'Tentacles', 2, 1),
-       ('Spongebob', 'Squarepants', 3, 1);
+VALUES 
+('Eugene', 'Krabs', 1, null),
+('Squidward', 'Tentacles', 2, 1),
+('Spongebob', 'Squarepants', 3, 1);
--- server.js
+++ server.js
@@ -77,33 +77,273 @@ const questions = [
           if (err) {
             console.log(err);
           }
-          console.log(results);
+          console.table(results);
+          init();
         });
       }
       
-      function viewAllRoles() {
-        console.log('Perform action for viewing all roles');
+function viewAllRoles() {
+  db.query(
+    `SELECT role.*, department.name AS department
+    FROM role
+    JOIN department ON role.department_id = department.id`,
+    function (err, results) {
+      if (err) {
+        console.log(err);
+        return;
       }
+      console.log(results)
+
+      const filteredResults = results.map(result => {
+        const { department_id, ...rest } = result;
+        return rest;
+      });
+
+      console.table(filteredResults);
+      init();
+    }
+  );
+}
       
       function viewAllEmployees() {
-        console.log('Perform action for viewing all employees');
-      }
+        db.query(
+          `SELECT employee.*, 
+          CASE 
+            WHEN employee.manager_id IS NULL THEN 'None'
+            ELSE CONCAT(manager.first_name, ' ', manager.last_name)
+          END AS manager,
+          role.title AS job_title,
+          department.name AS department,
+          role.salary
+          FROM employee
+          LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+          JOIN role ON employee.role_id = role.id
+          JOIN department ON role.department_id = department.id`,
+          function (err, results) {
+            if (err) {
+              console.log(err);
+            }
+            // Remove the role_id and manager_id columns from the results
+            const filteredResults = results.map(result => {
+              const { role_id, manager_id, ...rest } = result;
+              return rest;
+            });
       
+            console.table(filteredResults);
+            init();
+          }
+        );
+      }
+    
       function addDepartment() {
-        console.log('Perform action for adding a department');
+        inquirer.prompt({
+            type: 'input',
+            name:'name',
+            message:'What would you like to name this new department?'
+        })
+        .then((option) => {
+          const name = option.name;
+          console.log(name)
+          db.query('INSERT INTO department SET ?', { name: name }, (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            console.table(results);
+            init();
+          
+        });
+      })
+    }
+
+    //TODO: make it so that the choices are the departments and make the code convert that into its id
+   function addRole() {
+  const departments = `SELECT id, name FROM department`;
+
+  db.query(departments, (error, departmentResults) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'role',
+        message: 'What would you like to name this new role?'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary for this role?'
+      },
+      {
+        type: 'list',
+        name: 'departmentId',
+        message: 'What department is this role?',
+        choices: departmentResults.map((department) => ({ name: department.name, value: department.id }))
       }
+    ])
+    .then((option) => {
+      const role = option.role;
+      const salary = option.salary;
+      const department = option.departmentId;
+      console.log(role)
+      db.query('INSERT INTO role SET ?', { title: role, salary:salary, department_id:department }, (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.table(results);
+        init();
+      });
+    });
+  });
+}
       
-      function addRole() {
-        console.log('Perform action for adding a role');
+function addEmployee() {
+  // Query to retrieve the list of available managers
+  const managers = `SELECT CONCAT(first_name, ' ', last_name) AS manager_name, id FROM employee`;
+
+  // Query to retrieve the list of role titles and their corresponding role_ids
+  const roles = `SELECT title, id AS role_id FROM role`;
+
+  // Execute the queries
+  db.query(managers, (error, managerResults) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    db.query(roles, (error, roleResults) => {
+      if (error) {
+        console.error(error);
+        return;
       }
-      
-      function addEmployee() {
-        console.log('Perform action for adding an employee');
+
+      // Create the choices arrays
+      const roleChoices = roleResults.map(row => row.title);
+      const managerChoices = [
+        { name: "None", value: null },
+        ...managerResults.map(({ manager_name, id }) => ({
+          name: manager_name,
+          value: id,
+        })),
+      ];
+
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'first',
+            message: "What is the employee's first name?"
+          },
+          {
+            type: 'input',
+            name: 'last',
+            message: "What is the employee's last name?"
+          },
+          {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: roleChoices
+          },
+          {
+            type: 'list',
+            name: 'manager',
+            message: "Who is the employee's manager?",
+            choices: managerChoices
+          }
+        ])
+        .then(option => {
+          const first = option.first;
+          const last = option.last;
+          const roleTitle = option.role;
+          const manager = option.manager;
+
+          // Find the corresponding role_id for the selected role title
+          const selectedRole = roleResults.find(row => row.title === roleTitle);
+          const role = selectedRole ? selectedRole.role_id : null;
+
+          db.query(
+            'INSERT INTO employee SET ?',
+            { first_name: first, last_name: last, role_id: roleTitle, manager_id: manager },
+            (err, results) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              console.table(results);
+              init();
+            }
+          );
+        });
+    });
+  });
+}
+
+function updateEmployeeRole() {
+  const employeeList = `SELECT CONCAT(first_name, ' ', last_name) AS employee_name, id FROM employee`;
+  const roles = `SELECT title, id AS role_id FROM role`;
+
+  db.query(employeeList, (error, employeeResults) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    db.query(roles, (error, roleResults) => {
+      if (error) {
+        console.error(error);
+        return;
       }
-      
-      function updateEmployeeRole() {
-        console.log('Perform action for updating an employee role');
-      }
+
+      const employeeChoices = employeeResults.map(({ employee_name, id }) => ({
+        name: employee_name,
+        value: id,
+      }));
+
+      const roleChoices = roleResults.map(({ title, role_id }) => ({
+        name: title,
+        value: role_id,
+      }));
+
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'employeeId',
+            message: 'Select the employee to update:',
+            choices: employeeChoices,
+          },
+          {
+            type: 'list',
+            name: 'roleId',
+            message: 'Select the new role for the employee:',
+            choices: roleChoices,
+          },
+        ])
+        .then((option) => {
+          const employeeId = option.employeeId;
+          const roleId = option.roleId;
+
+          db.query(
+            'UPDATE employee SET role_id = ? WHERE id = ?',
+            [roleId, employeeId],
+            (err, results) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              console.log('Employee role updated successfully');
+              init();
+            }
+          );
+        });
+    });
+  });
+}
       
 // Hardcoded query: DELETE FROM course_names WHERE id = 3;
 
