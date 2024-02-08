d74bf6ffb8768911f8a79a301a4a0bf937aa4056
Ren3546 <128203284+Ren3546@users.noreply.github.com>
2024-02-07 20:36:52 -0500

    demo setup

--- server.js
+++ server.js
@@ -344,21 +344,5 @@ function updateEmployeeRole() {
     });
   });
 }
-      
-// Hardcoded query: DELETE FROM course_names WHERE id = 3;
-
-// db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-// // Query database
-// db.query('SELECT * FROM course_names', function (err, results) {
-//   console.log(results);
-// });
-
-// Default response for any other request (Not Found)
 
 init();
